a4e9a15b665227ce722e33d5cd8ce676042d4367
ruslan <4ruslan.k@gmail.com>
2020-10-24 16:11:07 +0500

    update config with domains

--- api/config.js
+++ api/config.js
@@ -1,11 +1,15 @@
 const config = {
+  local: {
+    APP_URL: 'http://localhost:3000',
+    API_URL: 'http://localhost:3600',
+  },
   development: {
     APP_URL: 'http://localhost:3000',
     API_URL: 'http://localhost:3600',
   },
   production: {
-    APP_URL: 'http://localhost:3000',
-    API_URL: 'http://localhost:3600',
+    APP_URL: 'https://fakedata.rk-projects.dev',
+    API_URL: 'https://api.fakedata.rk-projects.dev',
   },
 };
 
--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,12 @@
 const config = {
+  local: {
+    API_URL: 'http://localhost:3600',
+  },
   development: {
     API_URL: 'http://localhost:3600',
+  },
+  production: {
+    API_URL: process.env.REACT_APP_PRODUCTION_API_URL,
   },
 };
 
