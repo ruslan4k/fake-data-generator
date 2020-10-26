df2e55c9eb21757ed27d7a3d607dda50dae9441b
ruslan <4ruslan.k@gmail.com>
2020-10-26 15:15:44 +0500

    google analytics

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+/* eslint-disable no-undef */
+import React, { useEffect } from 'react';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
+import ReactGA from 'react-ga';
 import Routes from './features/Routes';
 import configureStore from './state/configureStore';
 import history from './helpers/history';
 
+history.listen((location) => {
+  ReactGA.pageview(location.pathname + location.search);
+});
+
 function App() {
+  useEffect(() => {
+    ReactGA.initialize('UA-181214830-1');
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
   const initialState = {};
   const store = configureStore(initialState, history);
   return (
